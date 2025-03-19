530a35224c41b17f7a5094b55d2d249e6f934820
Boyang Wang <wang.18492@buckeyemail.osu.edu>
2025-03-19 16:00:49 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 function toggleWelcomeLetter() {
-    let letter = document.getElementById("welcome-letter");
+    let expandedContent = document.getElementById("expanded-content");
     let button = document.getElementById("toggle-welcome-btn");
 
-    if (letter.style.display === "none" || letter.style.display === "") {
-        letter.style.display = "block";
+    if (expandedContent.style.display === "none" || expandedContent.style.display === "") {
+        expandedContent.style.display = "block";
         button.innerText = "Hide Welcome Letter";
     } else {
-        letter.style.display = "none";
-        button.innerText = "View Welcome Letter";
+        expandedContent.style.display = "none";
+        button.innerText = "Read More";
     }
 }
