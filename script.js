c72e7f6b7c00621922e7350efe1712cb78e18ef2
Boyang Wang <wang.18492@buckeyemail.osu.edu>
2025-03-19 11:55:23 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
 function showMessage() {
     document.getElementById("message").innerText = "You clicked the button!";
 }
+function showWelcomeLetter() {
+    let letter = document.getElementById("welcome-letter");
+    letter.style.display = "block";
+    document.getElementById("letter-content").innerHTML = `
+        Dear Learner,<br><br>
+        Welcome to the Electromagnetism Learning Website! Here, you'll find interactive lessons,
+        simulations, and explanations to help you understand the fundamental concepts of electromagnetism.<br><br>
+        Stay curious and keep exploring!<br><br>
+        Best regards,<br>
+        [Your Name]
+    `;
+}
