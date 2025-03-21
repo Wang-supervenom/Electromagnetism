f7301863dcf272eecb871a0ede7a66706c88de31
Boyang Wang <wang.18492@buckeyemail.osu.edu>
2025-03-21 11:59:44 -0400

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function toggleWelcomeLetter() {
     let expandedContent = document.getElementById("expanded-content");
     let button = document.getElementById("toggle-welcome-btn");
 
-    if (expandedContent.style.display === "none" || expandedContent.style.display === "") {
+    if (expandedContent.style.display === "none") {
         expandedContent.style.display = "block";
         button.innerText = "Hide Welcome Letter";
     } else {
